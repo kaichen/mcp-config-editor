9b9c652fe00cfbbdb9c463f080d10077ac7262dc
Kai <kai@thekaiway.com>
2024-11-27 16:01:52 +0800

    feat: integrate Tailwind CSS for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import "./index.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -41,7 +41,7 @@ function App() {
           onChange={(e) => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
-        <button type="submit">Greet</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Greet</button>
       </form>
       <p>{greetMsg}</p>
     </main>
